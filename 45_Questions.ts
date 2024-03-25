9a3ca16464193e82816704514526b773b31eb4fc
Parizah Shaikh <parivian6@gmail.com>
2024-03-26 00:58:15 +0500

    45_Questions_Assignment.

--- 45_Questions.ts
+++ 45_Questions.ts
@@ -1,4 +1,14 @@
-// Question No.1:  This is a simple "Hello World" program in typescript.
+// Question No.1:
+// This is a simple "Hello World" program in typescript.
 console.log("Hello World!");
 
-// Question No.2: 
+
+
+// Question No.2:
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+// Declare a variable to store the person's name.
+let PersonName = "Parizah Shaikh";
+
+// print the personalized message to the console.
+console.log(`Hello ${PersonName}, would you like to learn some Python today?`);
