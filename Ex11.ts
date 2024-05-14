894a782deaa0d7835974fa21511e2eb5edac3113
Parizah Shaikh <parivian6@gmail.com>
2024-05-14 20:42:15 +0500

    some changes.

--- Ex11.ts
+++ Ex11.ts
@@ -2,6 +2,7 @@
 // Names: Store the names of a few of your friends in a array called names.
 // Print each person’s name by accessing each element in the list, one at a time.
 
+// Answer:
 // Store names of friends in an array
 let friends = ["Arish", "Aries", "Parizah shaikh", "Hunaina"];
 
